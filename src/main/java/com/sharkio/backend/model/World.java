e05fb187defc3b79f1990e4e92dfe183c867607d
cmolinier <camille.molinier@etudiant.univ-rennes.fr>
2024-02-03 21:02:59 +0100

    remove cascade manyToMany

--- src/main/java/com/sharkio/backend/model/World.java
+++ src/main/java/com/sharkio/backend/model/World.java
@@ -19,7 +19,7 @@ public class World {
     @Column(name="y_dim")
     private float y_dim;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany
     @JoinTable(name = "World_Player",
     joinColumns = @JoinColumn(name = "world_id"),
     inverseJoinColumns = @JoinColumn(name = "player_id"))
