78e4b1ea495b53f23683150892ec33c14a77ec7e
cmolinier <camille.molinier@etudiant.univ-rennes1.fr>
2024-02-08 17:40:55 +0100

    regtangular checkbox

--- src/main/java/com/sharkio/backend/service/PlayerService.java
+++ src/main/java/com/sharkio/backend/service/PlayerService.java
@@ -5,6 +5,7 @@ import com.sharkio.backend.model.Player;
 import com.sharkio.backend.model.World;
 import com.sharkio.backend.repository.PlayerRepository;
 import com.sharkio.backend.repository.WorldRepository;
+import java.awt.geom.Rectangle2D;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,12 @@ public class PlayerService {
 
     private final double EATING_RANGE = 10;
     private final int SCORE_POINTS = 1;
-    private final float MOVE_CONSTRAINT = 300;
+    private final float MOVE_RANGE = 4;
+    private final float PLAYER_HITBOX_RANGE = 27;
+    private final int IMG_MID_WITH = 30;
+    private final int IMG_MID_HEIGHT = 17;
+
+    private final List<String> MOVE_ERRORS = new ArrayList<>(List.of("New coordinates are out of bound", "Invalid move, stop trying tp", "Can't move here, there is a collision"));
 
 
     public Iterable<Player> getPlayers() {
@@ -73,37 +79,61 @@ public class PlayerService {
         Player player = this.getById(id);
         World world = this.worldRepository.findAll().iterator().next();
 
+        int validation = is_valid_move(player, world, newX, newY, dt);
+        if(validation == 0) {
+            // Change value
+            player.setPos_x(newX);
+            player.setPos_y(newY);
+            this.repository.save(player); // Save to prevent other player to move here while current player is eating
+
+            // Check if player can eat something and perform action
+            Set<Food> foods = world.getFoods();
+            if(!foods.isEmpty()) {
+                this.eat(player, foods);
+            }
+
+            return this.getById(id);
+        }
+        throw new RuntimeException("Move invalid : " + this.MOVE_ERRORS.get(validation-1));
+    }
+
+    private boolean checkIfCollision(Player p, float newX, float newY) {
+        Rectangle2D rect1 = new Rectangle2D.Float(newX- getIMG_MID_WITH(), newY-
+            getIMG_MID_HEIGHT(), getIMG_MID_WITH()*2, getIMG_MID_HEIGHT()*2);
+        Rectangle2D rect2 = new Rectangle2D.Float(p.getPos_x()- getIMG_MID_WITH(), p.getPos_y()-
+            getIMG_MID_HEIGHT(), getIMG_MID_WITH()*2, getIMG_MID_HEIGHT()*2);
+
+        return rect1.intersects(rect2);
+    }
+
+    private int is_valid_move(Player player, World world, float newX, float newY, float dt) {
         // Assert coordinates are valid
         if(0 > newX || newX > world.getX_dim() || 0 > newY || newY > world.getY_dim()) {
-         throw new RuntimeException("New coordinates are out of bound");
+            return 1;
         }
 
-        // Assert move is valid
-        float move_range = this.MOVE_CONSTRAINT * dt;
-        if(!(player.getPos_x()-move_range <= newX && newX <= player.getPos_x()+move_range
-                && player.getPos_y()-move_range <= newY && newY <= player.getPos_y()+move_range)) {
-            throw new RuntimeException("Invalid move, stop trying tp");
+        // Assert no teleport
+        if((player.getPos_x()-this.MOVE_RANGE) > newX && newX > (player.getPos_x()+this.MOVE_RANGE) &&
+            (player.getPos_y()-this.MOVE_RANGE) > newY && newY > (player.getPos_y()+this.MOVE_RANGE)) {
+            return 2;
         }
 
-        // Change value
-        player.setPos_x(newX);
-        player.setPos_y(newY);
-        this.repository.save(player); // Save to prevent other player to move here while current player is eating
-
-        // Check if player can eat something and perform action
-        Set<Food> foods = world.getFoods();
-        if(!foods.isEmpty()) {
-            this.eat(player, foods);
+        // Check for collisions
+        for(Player p : world.getPlayers()) {
+            if(! p.getId().equals(player.getId())) {
+                if(checkIfCollision(p, newX, newY)) {
+                    return 3;
+                }
+            }
         }
-
-        return this.getById(id);
+        return 0;
     }
 
     private void eat(Player player, Set<Food> foods) {
         List<Integer> idsFoodsToRemove = new ArrayList<>();
 
         for (Food f : foods) {
-            if (computeDistanceToFood(player, f) < EATING_RANGE) {
+            if (computeDistance(player.getPos_x(), player.getPos_y(), f.getPos_x(), f.getPos_y()) < EATING_RANGE) {
                 idsFoodsToRemove.add(f.getId());
             }
         }
@@ -118,7 +148,7 @@ public class PlayerService {
         this.repository.save(player);
     }
 
-    private double computeDistanceToFood(Player player, Food f) {
-        return Math.sqrt(Math.pow((player.getPos_x()-f.getPos_x()),2)+Math.pow((player.getPos_y()-f.getPos_y()),2));
+    private float computeDistance(float x1, float y1, float x2, float y2) {
+        return (float) Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
     }
 }
